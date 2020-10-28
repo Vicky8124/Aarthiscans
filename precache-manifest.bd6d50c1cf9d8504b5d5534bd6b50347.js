66d8ab47515ea534d58228f3d9bbf2a6594975c8
AARTHISCANS <aarthiscans@AARTHISCANSs-Mac-mini-3.local>
2020-10-28 16:13:14 +0530

    Updates

---
+++ precache-manifest.bd6d50c1cf9d8504b5d5534bd6b50347.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb8d998114c144f6755",
+    "url": "/Aarthiscans/static/js/app.51f7bb86.chunk.js"
+  },
+  {
+    "revision": "263ac7b5255a5179b535",
+    "url": "/Aarthiscans/static/js/runtime~app.206368bb.js"
+  },
+  {
+    "revision": "add756e7520430001427",
+    "url": "/Aarthiscans/static/js/2.4b088ba2.chunk.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/Aarthiscans/static/media/back-icon-mask.c0e55859.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/Aarthiscans/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Aarthiscans/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/Aarthiscans/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/Aarthiscans/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Aarthiscans/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/Aarthiscans/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/Aarthiscans/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/Aarthiscans/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/Aarthiscans/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/Aarthiscans/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Aarthiscans/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/Aarthiscans/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "f7d8e7125fb092979d20ecac4f5fecea",
+    "url": "/Aarthiscans/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/Aarthiscans/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/Aarthiscans/expo-service-worker.js"
+  },
+  {
+    "revision": "edd548b8f73bf287f134a4f78ca63716",
+    "url": "/Aarthiscans/register-service-worker.js"
+  },
+  {
+    "revision": "e93fb5336d6078d04a42036925e5c6c1",
+    "url": "/Aarthiscans/static/js/2.4b088ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e450136bd117a2b3282128b5d4fc937e",
+    "url": "/Aarthiscans/favicon-16.png"
+  },
+  {
+    "revision": "2efab015c400010245381a632871bad4",
+    "url": "/Aarthiscans/favicon-32.png"
+  },
+  {
+    "revision": "2173119d8a4b748af9d094407f459841",
+    "url": "/Aarthiscans/favicon.ico"
+  },
+  {
+    "revision": "cdfcd44a92f290c1ba37aa6c8a5c6a6c",
+    "url": "/Aarthiscans/manifest.json"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Aarthiscans/precache-manifest.bd6d50c1cf9d8504b5d5534bd6b50347.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Aarthiscans/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
+
+workbox.routing.registerRoute(/^https?.*/, workbox.strategies.networkFirst({ "cacheName":"offlineCache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"purgeOnQuotaError":false})] }), 'GET');
